d8dc5fb98509e6dc2a0864cb6c63dba626dae4c2
kaidenvlr <kaidenvlr@gmail.com>
2023-10-15 19:44:45 +0600

    test coverage

--- apps/order_app/tests.py
+++ apps/order_app/tests.py
@@ -1,3 +1,98 @@
-from django.test import TestCase
+import json
+import os
 
-# Create your tests here.
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.urls import reverse, path, include
+from rest_framework.test import APITestCase, URLPatternsTestCase
+
+from apps.order_app.models import Order
+from core import settings
+
+
+class OrderTests(APITestCase, URLPatternsTestCase):
+    urlpatterns = [
+        path('api/user/', include('apps.user_app.urls')),  # User Application
+        path('api/order/', include('apps.order_app.urls')),  # Order Application
+    ]
+
+    def setUp(self):
+        self.username = "+77013926883"
+        self.password = "helloworld123@"
+        self.first_name = "Hello"
+        self.last_name = "World"
+        self.date_of_birth = "2002-04-03"
+        self.token = self.receive_token()
+        self.files = []
+
+        filenames = [
+            os.path.join(settings.BASE_DIR, 'media/orders/hearts.jpg'),
+            os.path.join(settings.BASE_DIR, 'media/orders/soyle.png')
+        ]
+        for filename in filenames:
+            f = open(filename, mode='rb')
+            fp = SimpleUploadedFile(name=filename, content=f.read(), content_type="image/jpg; image/png")
+            self.files.append(fp)
+
+    def api_authentication(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.token)
+
+    def receive_token(self):
+        response = self.client.post(
+            reverse("user_register"),
+            {
+                "username": self.username,
+                "password": self.password, "confirm_password": self.password,
+                "first_name": self.first_name, "last_name": self.last_name,
+                "date_of_birth": self.date_of_birth
+            }
+        )
+        self.assertEqual(201, response.status_code)
+        response = self.client.post(
+            reverse("user_login"),
+            {"username": self.username, "password": self.password}
+        )
+        self.assertEqual(200, response.status_code)
+        return json.loads(response.content).get('access_token')
+
+    def test_create_order(self):
+        self.api_authentication()
+        response = self.client.post(
+            reverse("add-order"),
+            {"images": self.files}
+        )
+        self.assertEqual(201, response.status_code)
+
+    def test_create_no_user_order(self):
+        response = self.client.post(
+            reverse("add-order"),
+            {"images": self.files}
+        )
+        self.assertEqual(201, response.status_code)
+
+    def test_get_order(self):
+        response = self.client.post(
+            reverse("add-order"),
+            {"images": self.files}
+        )
+        self.assertEqual(201, response.status_code)
+
+        order_id = int(json.loads(response.content).get("id"))
+        response = self.client.get(
+            reverse('get-order'),
+            {"order_id": order_id}
+        )
+        self.assertEqual(200, response.status_code)
+
+    def test_get_order_error(self):
+        response = self.client.get(
+            reverse("get-order"),
+            {"order_id": 1}
+        )
+        self.assertEqual(404, response.status_code)
+
+    def test_get_orders(self):
+        response = self.client.get(
+            reverse("get-orders")
+        )
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(len(json.loads(response.content)), Order.objects.count())
--- apps/user_app/tests.py
+++ apps/user_app/tests.py
@@ -1,3 +1,132 @@
-from django.test import TestCase
+import json
+import os
 
-# Create your tests here.
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.urls import reverse, path, include
+from rest_framework.test import APITestCase, URLPatternsTestCase
+
+from core import settings
+
+
+class UserTests(APITestCase, URLPatternsTestCase):
+    urlpatterns = [
+        path('api/user/', include('apps.user_app.urls')),  # User Application
+        path('api/order/', include('apps.order_app.urls')),  # Order Application
+    ]
+
+    def setUp(self):
+        self.token_user = "+77066342342"
+        self.token_password = "helloworld123@"
+        self.token_first_name = "Super"
+        self.token_last_name = "User"
+        self.token_date_of_birth = "2002-04-03"
+
+        self.username = "+77013926883"
+        self.password = "helloworld123@"
+        self.first_name = "Hello"
+        self.last_name = "World"
+        self.date_of_birth = "2002-04-03"
+        self.token = self.receive_token()
+        self.avatar = SimpleUploadedFile(
+            name="avatar_example.jpg",
+            content=open(os.path.join(settings.BASE_DIR, 'media/orders/hearts.jpg'), mode="rb").read(),
+            content_type="image/jpg"
+        )
+
+    def api_authentication(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.token)
+
+    def receive_token(self):
+        response = self.client.post(
+            reverse("user_register"),
+            {
+                "username": self.token_user,
+                "password": self.token_password, "confirm_password": self.token_password,
+                "first_name": self.token_first_name, "last_name": self.token_last_name,
+                "date_of_birth": self.token_date_of_birth
+            }
+        )
+        # print(response.content)
+        self.assertEqual(201, response.status_code)
+
+        response = self.client.post(
+            reverse("user_login"),
+            {"username": self.token_user, "password": self.token_password}
+        )
+        self.assertEqual(200, response.status_code)
+        return json.loads(response.content).get('access_token')
+
+    def test_create_user(self):
+        response = self.client.post(
+            reverse("user_register"),
+            {
+                "username": self.username,
+                "password": self.password, "confirm_password": self.password,
+                "first_name": self.first_name, "last_name": self.last_name,
+                "date_of_birth": self.date_of_birth
+            }
+        )
+        # print(response.content)
+        self.assertEqual(201, response.status_code)
+
+    def test_login_user(self):
+        response = self.client.post(
+            reverse("user_login"),
+            {"username": self.token_user, "password": self.token_password}
+        )
+        self.assertEqual(200, response.status_code)
+
+    def test_change_user(self):
+        self.api_authentication()
+        response = self.client.patch(
+            reverse("user_change_info"),
+            {
+                "first_name": "Hi!",
+                "date_of_birth": "2004-03-16",
+            }
+        )
+        self.assertEqual(202, response.status_code)
+
+    def test_change_user_error(self):
+        self.api_authentication()
+        response = self.client.patch(
+            reverse("user_change_info"),
+            {
+                "date_of_birth": "2023-01-01"
+            }
+        )
+        self.assertEqual(400, response.status_code)
+
+    def test_change_avatar(self):
+        self.api_authentication()
+        response = self.client.patch(
+            reverse("user_change_avatar"),
+            {
+                "avatar": self.avatar
+            }
+        )
+        self.assertEqual(202, response.status_code)
+
+    def test_get_user(self):
+        response = self.client.get(
+            reverse("user_get"),
+            {
+                "user_id": 1
+            }
+        )
+        self.assertEqual(200, response.status_code)
+
+    def test_get_user_error(self):
+        response = self.client.get(
+            reverse("user_get"),
+            {
+                "user_id": 126378
+            }
+        )
+        self.assertEqual(404, response.status_code)
+
+    def test_list_user(self):
+        response = self.client.get(
+            reverse("user_list")
+        )
+        self.assertEqual(200, response.status_code)
